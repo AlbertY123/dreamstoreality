555d99dc32ae87a236e26d842c701d6ba34b5e94
YaoFamily <albertyqyao@gmail.com>
2022-04-10 19:38:54 +1000

    New Update!

--- script1.js
+++ script1.js
@@ -23,7 +23,13 @@ function all(){
         document.getElementById("confirm").innerHTML = "We know that you are a " + name + " developer! Or to be more accurate, a dev that want's to learn "+ name + ". We are here to help :)";
         if(name=="html"){
             document.getElementById('step1').innerHTML = "Step 1, learn how to use " + name + "." + " Also install an IDE to help you get started.";
-            document.getElementById('step2').innerHTML = "Step 2, open up our IDE, the hardest bit about HTML is getting started. Don't worry, continue reading and we'll help";
+            document.getElementById('step2').innerHTML = "Step 2, open up your IDE, the hardest bit about HTML is getting started. Don't worry, continue reading and we'll help";
+            document.getElementById('step3').innerHTML = "Step 3, View some of these resources for more information.";
+        }
+        if(name=="css"){
+            document.getElementById('step1').innerHTML = "Step 1, learn how to use " + name + "." + " You can download An IDE or use an online website to get started.";
+            document.getElementById('step2').innerHTML = "Step 2, learn the basics. The real trick with CSS is to understand the basics. Don't worry, continue reading and we'll help";
+            document.getElementById('step3').innerHTML = "Step 3, View some of these resources for more information.";
         }
     }
     else{
